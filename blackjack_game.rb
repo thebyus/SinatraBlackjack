795bba879e36eb1596ba2d1f697ba516a182d4b9
Eamonn O'Brien <thebyusgames@gmail.com>
2013-06-20 16:17:01 -0700

    First commit of game
    First code commit for Blackjack in Sinatra

---
+++ blackjack_game.rb
@@ -0,0 +1,72 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+set :sessions, true
+
+helpers do
+	def calculate_total(cards)
+		arr = cards.map{|element| element[1]}
+
+		total = 0
+		arr.each do |a|
+			if a == "Ace"
+				total +=11
+			else
+				total += a.to_i == 0 ? 10 : a.to_i
+			end
+		end
+
+		#correct for Ace's
+		arr.select{|element| element == "Ace"}.count.times do
+			break if total <= 21
+			total -= 10
+		end
+
+		total
+	end
+end
+
+get '/home' do
+	if session[:player_name]
+		redirect '/game'
+	else
+		redirect '/new_player'
+	end
+end
+
+get '/new_player' do
+	erb :new_player
+end
+
+post '/new_player' do
+	session[:player_name] = params[:player_name]
+	redirect '/game'
+end
+
+get '/game' do
+	#create deck and put it in session
+	suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+	values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+	session[:deck] = suits.product(values).shuffle!
+	#	deal cards
+	session[:dealer_cards] = []
+	session[:player_cards] = []
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+
+	erb :game
+end
+
+post 'game/player/hit' do
+	#session[:player_cards] << session[:deck].pop
+	#erb :game
+puts "here we are!"
+end
+
+post 'game/player/stay' do
+
+	erb :game
+end

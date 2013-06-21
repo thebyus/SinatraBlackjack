3876925ca2c3ac92cc7adfdcca09dbab6282b00a
Eamonn O'Brien <thebyusgames@gmail.com>
2013-06-21 09:32:50 -0700

    visual game components

--- blackjack_game.rb
+++ blackjack_game.rb
@@ -10,7 +10,7 @@ helpers do
 
 		total = 0
 		arr.each do |a|
-			if a == "Ace"
+			if a == "ace"
 				total +=11
 			else
 				total += a.to_i == 0 ? 10 : a.to_i
@@ -18,12 +18,19 @@ helpers do
 		end
 
 		#correct for Ace's
-		arr.select{|element| element == "Ace"}.count.times do
+		arr.select{|element| element == "ace"}.count.times do
 			break if total <= 21
 			total -= 10
 		end
 
 		total
+	end
+
+	def card_image(card) #['Hearts', '4']
+		suit = card[0]
+		value = card[1]
+
+		"<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
 	end
 end
 
@@ -44,14 +51,19 @@ get '/new_player' do
 end
 
 post '/new_player' do
+	if params[:player_name].empty?
+		@error = "Name is required"
+		halt erb(:new_player)
+	end
+
 	session[:player_name] = params[:player_name]
 	redirect '/game'
 end
 
 get '/game' do
 	#create deck and put it in session
-	suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
-	values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+	suits = ['hearts', 'clubs', 'diamonds', 'spades']
+	values = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace']
 	session[:deck] = suits.product(values).shuffle!
 	#	deal cards
 	session[:dealer_cards] = []
@@ -67,9 +79,14 @@ end
 
 post '/game/player/hit' do
 	session[:player_cards] << session[:deck].pop
-	if calculate_total(session[:player_cards]) > 21
+	
+	player_total =calculate_total(session[:player_cards]) 
+	if player_total == 21
+			@success = "Blackjack! You win!"
+			@show_hit_or_stay = false
+	elsif player_total> 21
 		@error = "Sorry :( Looks like you have busted!"
-		@show_hit_or_stay =false
+		@show_hit_or_stay = false
 	end
 
 
